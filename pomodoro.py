9c855ca65f757e4b613e3496d5b9c58079b5f3c7
Thaïs Simões <thay.ssim@gmail.com>
2017-01-12 23:07:44 -0300

    Retirados métodos irrelevantes e adicionada função sleep
    Funcionando perfeitamente!

--- pomodoro.py
+++ pomodoro.py
@@ -2,41 +2,21 @@ import win32api
 import time
 
 class Pomodoro():
-    def __init__(self):
-        self.now = time.localtime(time.time())
-    def trabalho(self, tempoTrabalho):
-        self.minutoAtual = self.now.tm_min
-        self.minutoTrabalhoFinal = self.minutoAtual + tempoTrabalho
-        if self.minutoTrabalhoFinal >= 60:
-            self.minutoTrabalhoFinal= self.minutoTrabalhoFinal - 60
-            self.horaAtual= self.now.tm_hour+1
-        else:
-            self.horaAtual = self.now.tm_hour
-    def descanso(self, tempoDescanso):
-        self.minutoAtual = self.now.tm_min
-        self.minutoDescansoFinal = self.minutoAtual + tempoDescanso
-        if self.minutoDescansoFinal>= 60:
-            self.minutoDescansoFinal= self.minutoDescansoFinal - 60
-            self.horaAtual= self.now.tm_hour+1
-        else:
-            self.horaAtual = self.now.tm_hour
     def alertaTrabalho(self):
-        if self.now.tm_hour == self.horaAtual and self.now.tm_min == self.minutoTrabalhoFinal:
-            win32api.MessageBox(0, 'Seu horario de trabalho acabou!! Vai descansar!', "title", 0x00001000)
+            win32api.MessageBox(0, 'Seu horario de trabalho acabou!! Vai descansar!', "Free Pass \o/", 0x00001000)
     def alertaDescanso(self):
-        if self.now.tm_hour == self.horaAtual and self.now.tm_min == self.minutoDescansoFinal:
-            win32api.MessageBox(0, "Seu horario de descanso acabou!!", "title", 0x00001000)
+            win32api.MessageBox(0, "Seu horario de descanso acabou!! Vai trabalhar!", "Caminhao de trabalho", 0x00001000)
 
 
 pomodoro = Pomodoro()
-tempoDeDescanso = 0
-tempoDeTrabalho = 1
+tempoDeDescanso = 5
+tempoDeTrabalho = 25
 
 while True:
-    pomodoro.trabalho(tempoDeTrabalho)
+
+    time.sleep(tempoDeTrabalho*60)
     pomodoro.alertaTrabalho()
-    pomodoro.descanso(tempoDeDescanso)
-    #elif pomodoro.descanso(tempoDeDescanso):
-    #    pomodoro.alertaDescanso()
-     #   pomodoro.trabalho(tempoDeTrabalho)
+    time.sleep(tempoDeDescanso * 60)
+    pomodoro.alertaDescanso()
+
 
